87c4786dad06853965d5f55c9be5e0aa05ceab33
stinja <projectmaydev@gmail.com>
2021-09-20 14:22:38 -0500

    Initial commit

---
+++ src/Quote.java
@@ -0,0 +1,19 @@
+public class Quote { // class declaration
+
+    int index;
+    String quoteContent;
+    String quoteAuthor;
+
+    // constructors
+    public Quote() {
+    }
+
+    public Quote(int index) {
+    }
+
+    public Quote(String quoteContent) {
+    }
+
+    public Quote(int index, String quoteContent, String quoteAuthor) {
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Main {
+
+    /** FUNCTIONS/METHODS
+     *
+     * createQuote - create a single quote based on user input
+     * deleteQuote - given the index of the quote, delete from text file
+     * editQuote - given the index, grab quote and allow user to edit
+     *
+     *
+     * addQuoteToTextFile - append quote to text file
+     * deleteQuoteFromTextFile
+     * readQuoteFromTextFile - read a single quote from text based on index
+     * readAllQuotesFromTextFile - read all quotes from text
+     * printQuoteToScreen - print read quote to screen
+     * printAllQuotesToScreen - print read quotes to screen
+     *
+     *
+     * getIndex
+     * getQuoteContent
+     * getQuoteAuthor
+     * getDateAdded
+     *
+     * setIndex
+     * setQuoteContent
+     * setDateAdded
+     *
+     */
+
+    /** TODO Attempt to make a quote object
+     *
+     * quoteIndex
+     * quoteContent
+     * quoteAuthor
+     * **/
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
